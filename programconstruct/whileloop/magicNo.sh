9d5b0c38709d6f594cb1cc31efecba3bac42ed7c
pranjalikawale <pranjalikawale1578@gmail.com>
2020-03-25 10:00:56 -1200

    MODIFIED: Magic numbe in while loop

--- programconstruct/whileloop/magicNo.sh
+++ programconstruct/whileloop/magicNo.sh
@@ -1,14 +1,32 @@
 #!/bin/bash -x
 
-read -p "Read the number" num
-
 MAX_LIMIT=100
 MIN_LIMIT=0
 
-if [[ $num -gt $MIN_LIMIT && $num -lt $MAX_LIMIT ]]
-then
-	if [[ (($num -lt $(($num/2)))) || (($num -gt $(($num/2))))]]
-	then
-		echo "hi"
-	fi
-fi
+middle=0
+choice=0
+
+while [[(($choice -le 3))]]
+do
+	middle=$(( ($MIN_LIMIT+$MAX_LIMIT)/2 ))
+
+	echo "1.Number greater than $middle"
+	echo "2.Number less han $middle"
+	echo "3.Number equal to $middle"
+
+	read -p "Read the choice" choice
+
+	case $choice in 
+	1)
+		MIN_LIMIT=$(($middle+1))
+		;;
+	2)
+		MAX_LIMIT=$(($middle-1))
+		;;
+	3)
+		echo "I gussed your number $middle"
+		exit
+		;;
+	esac
+done
+
