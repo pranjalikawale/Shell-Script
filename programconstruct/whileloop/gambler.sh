bd42757e72f34976cac87e65b67a382a04fe211f
pranjalikawale <pranjalikawale1578@gmail.com>
2020-03-25 10:32:21 -1200

    ADDED: Gambler program in while loop

---
+++ programconstruct/whileloop/gambler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+money=100
+won=0
+bet=0
+
+while [[ (($money -gt 0 )) && (($money -ne 200)) ]]
+do
+	val=$(( $(( RANDOM % $(($MAX -$MIN+1)) )) + $MIN ))
+	if (( $val -eq 1 ))
+	then	
+		((money++))
+		((won++))
+	else
+		((money--))
+	fi
+((bet++))
+done
+
+echo "Number of time u won is $won"
+echo "Number of time bet is $bet"
